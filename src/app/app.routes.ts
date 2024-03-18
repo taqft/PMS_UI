e9163625fa38505800819741bcc94f0999fbea21
Ben Koenig <70115233+taqft@users.noreply.github.com>
2024-03-18 10:21:03 -0600

    init; routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'products',
+        component: ProductsComponent
+    },
+    {
+        path: 'products/add',
+        component: AddProductComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
